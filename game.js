77b546fcd7c45d4797bd24b776313ea869427ddc
Dusmok <54205254+Dusmok@users.noreply.github.com>
2020-06-15 12:32:22 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,109 @@
+
+var buttonColors = ['red', 'blue', 'green', 'yellow'];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+
+
+
+
+function nextSequence() {
+  userClickedPattern=[];
+$('body').css('background', '#011F3F')
+var randomNumber = (Math.floor(Math.random()*4));
+var randomChosenColor = buttonColors[randomNumber];
+
+gamePattern.push(randomChosenColor);
+
+playSound(randomChosenColor)
+
+$("."+randomChosenColor).fadeToggle(100).fadeToggle(100)
+
+level++
+  $('h1').text ("Level " + level).css('color', '#FEF2BF')
+}
+
+
+$('.btn').on('click',function () {
+  var userClickedColor = this.id
+userClickedPattern.push(userClickedColor) ;
+ playSound(userClickedColor)
+ $(this).addClass('pressed');
+ setTimeout(function(){$('.btn').removeClass('pressed');}, 100)
+
+checkAnswer(userClickedPattern.length-1);
+});
+
+
+
+function playSound(name) {
+
+  var colorAudio = new Audio('sounds/'+name+'.mp3');
+  colorAudio.play();
+
+}
+
+
+function animatePress(currentColor) {
+
+}
+
+var start = false;
+
+if (start === false){
+
+$(document).on('keydown', (function () {
+  nextSequence();
+ start = true;
+$(document).off('keydown')
+}))
+}
+
+
+
+
+function checkAnswer(currentLevel) {
+
+if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {console.log('good')
+
+  if (gamePattern.length === userClickedPattern.length) {setTimeout(function(){nextSequence()},1000)}
+}
+
+
+
+else {
+  $('.btn').off('click')
+  console.log('bad')
+  playSound('wrong')
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+  start = true;
+  $('#dodatek').removeClass('hidden')
+  $('h1').text('PRZEGRAŁEŚ!').css('color', 'white')
+  $('body').css('background', 'red')
+  $('body').on('keydown', (function () {
+
+    nextSequence();
+
+  $('body').off('keydown')
+  $('#dodatek').addClass('hidden')
+
+  $('.btn').on('click',function klikando() {
+    var userClickedColor = this.id
+  userClickedPattern.push(userClickedColor) ;
+   playSound(userClickedColor)
+   $(this).addClass('pressed');
+   setTimeout(function(){$('.btn').removeClass('pressed');}, 100)
+
+  checkAnswer(userClickedPattern.length-1);
+
+  });
+
+
+  }))
+
+  }
+
+};
