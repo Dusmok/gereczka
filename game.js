7f977c670d2bc5b3ca2c2e8cc7871cfca161a299
Dusmok <54205254+Dusmok@users.noreply.github.com>
2020-06-15 15:48:58 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,23 +1,25 @@
-
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
 var userClickedPattern = [];
 var level = 0;
 
 
-$("#dodatek").on('click', function () {
+$("#dodatek").on('click', function() {
   nextSequence();
- $('#dodatek').addClass('hidden')
+  $('#dodatek').addClass('hidden')
 
-    $('.btn').on('click',function () {
-      var userClickedColor = this.id
-    userClickedPattern.push(userClickedColor) ;
-     playSound(userClickedColor)
-     $(this).addClass('pressed');
-     setTimeout(function(){$('.btn').removeClass('pressed');}, 100)
+  $('.btn').on('click', function() {
 
-    checkAnswer(userClickedPattern.length-1);
-    });
+    var userClickedColor = this.id
+    userClickedPattern.push(userClickedColor);
+    playSound(userClickedColor)
+    $(this).addClass('pressed');
+    setTimeout(function() {
+      $('.btn').removeClass('pressed');
+    }, 100)
+
+    checkAnswer(userClickedPattern.length - 1);
+  });
 
 
 
@@ -29,19 +31,19 @@ function nextSequence() {
 
 
   $('#dodatek').off('click')
-  userClickedPattern=[];
-$('body').css('background', '#011F3F')
-var randomNumber = (Math.floor(Math.random()*4));
-var randomChosenColor = buttonColors[randomNumber];
+  userClickedPattern = [];
+  $('body').css('background', '#011F3F')
+  var randomNumber = (Math.floor(Math.random() * 4));
+  var randomChosenColor = buttonColors[randomNumber];
 
-gamePattern.push(randomChosenColor);
+  gamePattern.push(randomChosenColor);
 
-playSound(randomChosenColor)
+  playSound(randomChosenColor)
 
-$("."+randomChosenColor).fadeToggle(100).fadeToggle(100)
+  $("." + randomChosenColor).fadeToggle(100).fadeToggle(100)
 
-level++
-  $('h1').text ("Level " + level).css('color', '#FEF2BF')
+  level++
+  $('h1').text("Level " + level).css('color', '#FEF2BF')
 }
 
 
@@ -51,7 +53,7 @@ level++
 
 function playSound(name) {
 
-  var colorAudio = new Audio('sounds/'+name+'.mp3');
+  var colorAudio = new Audio('sounds/' + name + '.mp3');
   colorAudio.play();
 
 }
@@ -79,59 +81,63 @@ function checkAnswer(currentLevel) {
 
 
 
-if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {console.log('good')
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log('good')
 
 
-  if (gamePattern.length === userClickedPattern.length) {setTimeout(function(){nextSequence()},1000)}
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(function() {
+        nextSequence()
+      }, 1000)
+    }
 
 
-}
+  } else {
+    $('.btn').off('click')
+    console.log('bad')
+    playSound('wrong')
+    gamePattern = [];
+    userClickedPattern = [];
+    level = 0;
+    start = true;
+    $('#dodatek').removeClass('hidden')
+    $('#dodatek').on('click', function() {
+      nextSequence()
+$('#dodatek').addClass('hidden')
+      $('.btn').on('click', function() {
+        var userClickedColor = this.id
+        userClickedPattern.push(userClickedColor);
+        playSound(userClickedColor)
+        $(this).addClass('pressed');
+        setTimeout(function() {
+          $('.btn').removeClass('pressed');
+        }, 100)
 
-
-
-else {
-  $('.btn').off('click')
-  console.log('bad')
-  playSound('wrong')
-  gamePattern = [];
-  userClickedPattern = [];
-  level = 0;
-  start = true;
-  $('#dodatek').removeClass('hidden')
-  $('#dodatek').on('click', function(){nextSequence()
-
-        $('.btn').on('click',function () {
-          var userClickedColor = this.id
-        userClickedPattern.push(userClickedColor) ;
-         playSound(userClickedColor)
-         $(this).addClass('pressed');
-         setTimeout(function(){$('.btn').removeClass('pressed');}, 100)
-
-        checkAnswer(userClickedPattern.length-1);
-        });
-  })
-  $('h1').text('PRZEGRAŁEŚ!').css('color', 'white')
-  $('body').css('background', 'red')
-  // $('body').on('keydown', (function () {
-  //
-  //   nextSequence();
-  //
-  // $('body').off('keydown')
-  // $('#dodatek').addClass('hidden')
-  //
-  // $('.btn').on('click',function klikando() {
-  //   var userClickedColor = this.id
-  // userClickedPattern.push(userClickedColor) ;
-  //  playSound(userClickedColor)
-  //  $(this).addClass('pressed');
-  //  setTimeout(function(){$('.btn').removeClass('pressed');}, 100)
-  //
-  // checkAnswer(userClickedPattern.length-1);
-  //
-  // });
-  //
-  //
-  // }))
+        checkAnswer(userClickedPattern.length - 1);
+      });
+    })
+    $('h1').text('PRZEGRAŁEŚ!').css('color', 'white')
+    $('body').css('background', 'red')
+    // $('body').on('keydown', (function () {
+    //
+    //   nextSequence();
+    //
+    // $('body').off('keydown')
+    // $('#dodatek').addClass('hidden')
+    //
+    // $('.btn').on('click',function klikando() {
+    //   var userClickedColor = this.id
+    // userClickedPattern.push(userClickedColor) ;
+    //  playSound(userClickedColor)
+    //  $(this).addClass('pressed');
+    //  setTimeout(function(){$('.btn').removeClass('pressed');}, 100)
+    //
+    // checkAnswer(userClickedPattern.length-1);
+    //
+    // });
+    //
+    //
+    // }))
 
   }
 
